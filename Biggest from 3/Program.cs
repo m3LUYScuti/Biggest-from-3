927e8a072dd085a11ff7e727f5d12fb8c9a9cf5e
davit <davit@GOSH>
2024-09-24 16:57:05 +0400

    Add project files.

---
+++ Biggest from 3/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Number 1:");
+
+int Num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Number 2:");
+int Num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Number 3:");
+int Num3 = int.Parse(Console.ReadLine());
+
+if (Num1 < Num3 && Num2 < Num3)
+{
+    Console.WriteLine("Number 3 is biggest");
+}
+else if (Num1 < Num2 && Num2 > Num3)
+{
+    Console.WriteLine("Number 2 is biggest");
+}
+else if (Num1 > Num2 && Num1 > Num3)
+{
+    Console.WriteLine("Number 2 is biggest");
+}
+else if (Num1 == Num2 && Num3 == Num1) 
+{
+    Console.WriteLine("Numbers are equal");
+}
+
+
+else
+    Console.WriteLine("Wrong");
+
+Console.ReadLine();
